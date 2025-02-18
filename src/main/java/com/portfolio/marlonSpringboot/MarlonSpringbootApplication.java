f7c76b0e8c39e10fdbeafc15200ffe75e8dd7447
marlon tolentino <tolentino.marlon.agustin@gmail.com>
2025-02-18 11:24:25 +0800

    push all spring boot generated files

---
+++ src/main/java/com/portfolio/marlonSpringboot/MarlonSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.marlonSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarlonSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarlonSpringbootApplication.class, args);
+	}
+
+}
