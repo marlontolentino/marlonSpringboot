b0224123a634f7b238563aed7b202e2fbda71d1e
marlon tolentino <tolentino.marlon.agustin@gmail.com>
2025-02-20 22:17:19 +0800

    [Login Screen] Initial commit

--- src/main/java/com/portfolio/marlonSpringboot/controllers/LoginController.java
+++ src/main/java/com/portfolio/marlonSpringboot/controllers/LoginController.java
@@ -2,6 +2,7 @@ package com.portfolio.marlonSpringboot.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -11,6 +12,11 @@ public class LoginController {
 	@GetMapping("/login")
 	public String login() {
 		return "/login";
+	}
+
+	@PostMapping("/login")
+	public String submit() {
+		return "redirect:/home";
 	}
 
 	@GetMapping("/testURL")
