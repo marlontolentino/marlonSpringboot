bf8f7e6fef992dcafdfa08380b09086d54ae2316
marlon tolentino <tolentino.marlon.agustin@gmail.com>
2025-03-05 00:01:12 +0800

    [Login Screen] changes in screen layout add thymeleaf

--- src/main/java/com/portfolio/marlonSpringboot/forms/LoginForm.java
+++ src/main/java/com/portfolio/marlonSpringboot/forms/LoginForm.java
@@ -2,11 +2,22 @@ package com.portfolio.marlonSpringboot.forms;
 
 import java.io.Serializable;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginForm implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 2727474026933352011L;
+
+	/** Logged-in username */
+	private String username;
+
+	/** Logged-in password */
+	private String password;
 
 }
